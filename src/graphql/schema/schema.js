e4b1512160210db23301e358af3b7fe991cc5879
Michael R <tekmedias@gmail.com>
2022-06-29 09:42:07 +0200

    :hammer: Add Query type on buildSchema

--- src/graphql/schema/schema.js
+++ src/graphql/schema/schema.js
@@ -2,6 +2,10 @@ const {buildSchema} = require('graphql')
 // buildSchema va nous permettre de déclarer notre schema au sein de graphQL
 
 const Schema = buildSchema(`
+  type Query {
+    movies: [Movie!]!
+  }
+  
   type Movie {
     _id: ID!,
     title: String!,
