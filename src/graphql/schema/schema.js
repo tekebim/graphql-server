320d6eb991f162ef59419bc5ca49cc2c8ae89424
Michael R <tekmedias@gmail.com>
2022-06-29 09:38:55 +0200

    :hammer: Add Movie type on Schema

--- src/graphql/schema/schema.js
+++ src/graphql/schema/schema.js
@@ -1,2 +1,11 @@
-const { buildSchema } = require('graphql')
+const {buildSchema} = require('graphql')
 // buildSchema va nous permettre de déclarer notre schema au sein de graphQL
+
+const Schema = buildSchema(`
+  type Movie {
+    _id: ID!,
+    title: String!,
+    plot: String!,
+    createdAt: String!
+  }
+`);
